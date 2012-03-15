7d5c64b0cdcb9f72ca2bb96e22b81933f9cf0cfe
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-15 20:47:48 +0100

    Leiningen new
    the leingingen is used to generate the project.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject levenshtein "1.0.0-SNAPSHOT"
+  :description "Levenshtein determines the edit distance between two strings"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ test/levenshtein/test/core.clj
@@ -0,0 +1,6 @@
+(ns levenshtein.test.core
+  (:use [levenshtein.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
